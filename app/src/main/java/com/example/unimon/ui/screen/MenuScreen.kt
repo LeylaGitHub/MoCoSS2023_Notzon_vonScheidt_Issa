a69533a705ed7ad97b84eb3ae9ff0a6ee13c954b
Alessa-v-Scheidt <avscheidt49@gmail.com>
2023-05-16 13:52:19 +0200

    updated layout for menu

--- app/src/main/java/com/example/unimon/ui/screen/MenuScreen.kt
+++ app/src/main/java/com/example/unimon/ui/screen/MenuScreen.kt
@@ -1,7 +1,9 @@
 package com.example.unimon.ui.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,14 +13,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.LineBreak
@@ -36,9 +41,11 @@ fun MenuScreen(
 ) {
     Column(modifier = Modifier
         .fillMaxSize()
+        .paint(painterResource(R.drawable.menu), contentScale = ContentScale.FillBounds)
+        .padding(0.dp, 20.dp, 0.dp, 0.dp)
         ) {
         Steps()
-        Settings(navigateToHome)
+        Actions(navigateToHome)
         Box(modifier = Modifier
             .weight(1f)
             .fillMaxSize()
@@ -55,7 +62,8 @@ fun Steps() {
     Column(
         Modifier
             .fillMaxWidth()
-            .padding(20.dp)
+            .padding(20.dp, 0.dp, 20.dp, 100.dp)
+            .background(Color.White, RoundedCornerShape(20.dp))
     ) {
     Text(
         "Schritte: ",
@@ -64,17 +72,17 @@ fun Steps() {
         color = Color.Black,
         modifier = Modifier
             .align(Alignment.Start)
-            .padding(20.dp)
+            .padding(15.dp)
     )
 }
 }
 
 @Composable
-fun Settings(navigateToHome: () -> Unit) {
+fun Actions(navigateToHome: () -> Unit) {
     Column(
         Modifier
             .fillMaxWidth()
-            .padding(20.dp),
+            .padding(20.dp, 0.dp),
         Arrangement.Center
     ) {
         MenuPoint(R.drawable.light, "Light on/off", navigateToHome)
@@ -91,23 +99,23 @@ fun MenuPoint(imageId : Int, underlineText : String, navigateToHome: () -> Unit)
             .padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Surface(modifier = Modifier
-            .size(90.dp,90.dp)
-            .border(3.dp, Color.Black, RoundedCornerShape(15.dp))
-        ) {
-            FloatingActionButton(
-                onClick = navigateToHome,
-                modifier = Modifier
-                    .size(90.dp, 90.dp),
-                containerColor = Color.White
-            ) {
-                Image(
-                    painterResource(imageId),
-                    "placeholder",
-                    Modifier
-                        .size(90.dp)
-                )
-            }
+        Button(
+            onClick = navigateToHome,
+            modifier = Modifier
+                .size(90.dp,90.dp)
+                .border(3.dp, Color.Black, RoundedCornerShape(17.dp))
+                .clickable(onClick = navigateToHome)
+                .padding(0.dp),
+            colors = ButtonDefaults.buttonColors(Color.White),
+            shape = RoundedCornerShape(20.dp)
+        )
+        {
+            Image(
+                painterResource(imageId),
+                "placeholder",
+                Modifier
+                    .size(90.dp)
+            )
         }
         Text(
             text = underlineText,
