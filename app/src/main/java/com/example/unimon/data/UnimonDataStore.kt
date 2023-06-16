0d95ad8a0ff21f1fd17a54c5ab0e1b7bf4eac4a5
Alessa-v-Scheidt <avscheidt49@gmail.com>
2023-06-16 18:45:31 +0200

    added remaining stats to DataStore

--- app/src/main/java/com/example/unimon/data/UnimonDataStore.kt
+++ app/src/main/java/com/example/unimon/data/UnimonDataStore.kt
@@ -3,12 +3,30 @@ package com.example.unimon.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "unimon")
+
+val UNIMON_NAME_KEY = "name"
+val UNIMON_NAME = stringPreferencesKey(UNIMON_NAME_KEY)
+
+fun getUnimonName(context: Context): Flow<String> {
+    return context.dataStore.data.map { unimonData ->
+        unimonData[UNIMON_NAME] ?: "Koko"
+    }
+}
+
+//suspend fun updateName(context: Context) {
+//    context.dataStore.edit { unimonData ->
+//        val currentName = unimonData[UNIMON_NAME] ?: "Koko"
+//        unimonData[UNIMON_NAME] = currentCount + 1
+//    }
+//}
 
 val UNIMON_LEVEL_KEY = "level"
 val UNIMON_LEVEL = intPreferencesKey(UNIMON_LEVEL_KEY)
@@ -18,3 +36,25 @@ fun getUnimonLevel(context: Context): Flow<Int> {
         unimonData[UNIMON_LEVEL] ?: 1
     }
 }
+
+suspend fun levelUp(context: Context) {
+    context.dataStore.edit { unimonData ->
+        val currentCount = unimonData[UNIMON_LEVEL] ?: 1
+        unimonData[UNIMON_LEVEL] = currentCount + 1
+    }
+}
+
+val UNIMON_BODY_KEY = "level"
+val UNIMON_BODY = intPreferencesKey(UNIMON_BODY_KEY)
+
+val UNIMON_MIND_KEY = "level"
+val UNIMON_MIND = intPreferencesKey(UNIMON_MIND_KEY)
+
+val UNIMON_SOCIAL_KEY = "level"
+val UNIMON_SOCIAL = intPreferencesKey(UNIMON_SOCIAL_KEY)
+
+val UNIMON_SLEEP_KEY = "level"
+val UNIMON_SLEEP= intPreferencesKey(UNIMON_SLEEP_KEY)
+
+
+
